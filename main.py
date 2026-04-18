34519223d1c6f5bc2a77c6e0c5b120b0570153db
Hail808 <pdecosta28@gmail.com>
2026-04-17 18:31:21 -0700

    Added input functionality to add books via terminal

--- main.py
+++ main.py
@@ -3,16 +3,4 @@ import tracker
 print("Initializing...")
 tracker.create_library()
 
-test_book1 = {
-         "id": 1,
-         "name": "Oregairu"
-    }
-
-tracker.add_to_library(test_book1)
-
-test_book2 = {
-         "id": 2,
-         "name": "Oshi no Ko"
-    }
-
-tracker.add_to_library(test_book2)
+tracker.new_book()
--- tracker.py
+++ tracker.py
@@ -10,16 +10,40 @@ def create_library():
         with open(file_name, "w") as json_file:
             json.dump(library, json_file, indent=4)
 
-def add_to_library(book):
-    books = load_books()
-    books.append(book)
-    save_books(books)
+def new_book():
+    new_book = read_input()
 
-def load_books():
+    cur_library = load_library()
+    new_id = len(cur_library) + 1
+    new_book["id"] = new_id
+
+    add_to_library(new_book, cur_library)
+
+def read_input():
+    book_title = input("Enter book title: ")
+    book_author = input("Enter author: ")
+    book_status = input("Enter reading status: ")
+    book_curVol = input("Enter the current volume you are on: ")
+    book_curCh = input("Enter the current chapter you are on: ")
+    book = {
+        "id": 0,
+        "title": book_title,
+        "author": book_author,
+        "status": book_status,
+        "currentVol": book_curVol,
+        "currentCh": book_curCh
+    }
+    return book
+
+def add_to_library(book, cur_library):
+    cur_library.append(book)
+    save_library(cur_library)
+
+def load_library():
     with open(file_name, "r") as file:
         library_data = json.load(file)
     return library_data
 
-def save_books(books):
+def save_library(cur_library):
     with open(file_name, "w") as file:
-        json.dump(books, file, indent=4)
+        json.dump(cur_library, file, indent=4)
