a732601c35bca60ea3179e95c93f6a2fec749aa4
Hail808 <pdecosta28@gmail.com>
2026-04-17 17:53:43 -0700

    add books, load library, and save library functions

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import tracker
 
 print("Initializing...")
 tracker.create_library()
+
+test_book1 = {
+         "id": 1,
+         "name": "Oregairu"
+    }
+
+tracker.add_to_library(test_book1)
+
+test_book2 = {
+         "id": 2,
+         "name": "Oshi no Ko"
+    }
+
+tracker.add_to_library(test_book2)
--- tracker.py
+++ tracker.py
@@ -1,9 +1,25 @@
 import json
 import os
 
+library = []
+file_name = "library.json"
+
 def create_library():
     # create only if library.json doesn't already exist
-    if not os.path.exists("library.json"):
-        data = []
-        with open("library.json", "w") as json_file:
-            json.dump(data, json_file, indent=2)
+    if not os.path.exists(file_name):
+        with open(file_name, "w") as json_file:
+            json.dump(library, json_file, indent=4)
+
+def add_to_library(book):
+    books = load_books()
+    books.append(book)
+    save_books(books)
+
+def load_books():
+    with open(file_name, "r") as file:
+        library_data = json.load(file)
+    return library_data
+
+def save_books(books):
+    with open(file_name, "w") as file:
+        json.dump(books, file, indent=4)
